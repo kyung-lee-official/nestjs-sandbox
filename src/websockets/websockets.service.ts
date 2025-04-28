f968bb85af428775c4303666f41b7d7459a567e6
Kyung Lee <kyung.lee.official@outlook.com>
2025-04-28 22:02:54 +0800

    refactor: update WebsocketsGateway and WebsocketsService to enhance message handling and connection logging

--- src/websockets/websockets.service.ts
+++ src/websockets/websockets.service.ts
@@ -1,54 +1,36 @@
 import { Injectable } from "@nestjs/common";
 import { Socket } from "socket.io";
 
+export type MsgBody = {
+	message: string;
+};
+
 @Injectable()
 export class WebsocketsService {
-	message(body: any, clientSocket: Socket) {
+	singletonTaskProgressValue: number = 0;
+	chatHistory: string[] = [];
+
+	sentToRoom(body: MsgBody, clientSocket: Socket) {
 		try {
 			const { message } = body;
-			/* log the message coming from the client */
-			console.log(
-				`Client ${clientSocket.id}: ${JSON.stringify(message)}`
-			);
-			/* 8s is long enough for testing multiple clients */
-			const processingTime = 8;
-			for (let i = 0; i < processingTime; i++) {
-				setTimeout(() => {
-					clientSocket.emit("progress", {
-						progress: (i + 1) / processingTime,
-					});
-					if (i === processingTime - 1) {
-						clientSocket.disconnect();
-					}
-				}, 1000 * i);
-			}
+			this.chatHistory.push(`Client ${clientSocket.id}: ${message}`);
+			/**
+			 * emit the message to all connected sockets within specified namespace (nsp)
+			 *
+			 * why not use clientSocket.broadcast.emit?
+			 * The broadcast method of a Socket instance emits an event to all other
+			 * connected clients in the same namespace except the sender.
+			 */
+			clientSocket.nsp.emit("s2c", this.chatHistory);
 		} catch (error) {
 			if (error instanceof Error) {
 				console.error(`Error processing message: ${error.message}`);
 			} else {
 				console.error("Unknown error occurred");
 			}
-			clientSocket.emit("error", { message: "An error occurred" });
+			clientSocket.emit("error", {
+				message: "An error occurred on the server",
+			});
 		}
 	}
-
-	// create(createWebsocketDto: CreateWebsocketDto) {
-	// 	return "This action adds a new websocket";
-	// }
-
-	// findAll() {
-	// 	return `This action returns all websockets`;
-	// }
-
-	// findOne(id: number) {
-	// 	return `This action returns a #${id} websocket`;
-	// }
-
-	// update(id: number, updateWebsocketDto: UpdateWebsocketDto) {
-	// 	return `This action updates a #${id} websocket`;
-	// }
-
-	// remove(id: number) {
-	// 	return `This action removes a #${id} websocket`;
-	// }
 }
