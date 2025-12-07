d9f6cfa7bd4e3bfc1f4486054788285d7497acc2
Kyung Lee <kyung.lee.official@outlook.com>
2025-12-07 18:06:22 +0800

    wip(database): update Prisma client output path and restructure exports

--- packages/database/src/index.ts
+++ packages/database/src/index.ts
@@ -1,18 +1,3 @@
-import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "./client/client.js";
-
-const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL! });
-
-// The client is instantiated once here, ready to be used
-const prisma = new PrismaClient({
-  adapter,
-});
-
-// Export everything from the generated client (includes all models and types)
-export { PrismaClient } from "./client/client.js";
-export * from "./client/enums.js";
-export * from "./client/models.js";
-
-// Export the configured prisma instance
-export { prisma };
-export default prisma;
+// packages/database/src/index.ts
+export { prisma } from "./client.js";
+export * from "./generated/client/client.js"; // or "../generated/prisma/client"
