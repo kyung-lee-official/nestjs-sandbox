8d66c91617cc925e86e4b25c7b470d597dd5a5ff
Kyung Lee <kyung.lee.official@outlook.com>
2025-12-12 22:00:09 +0800

    feat: add Medusa error types mapping and update exports in types package

--- packages/types/src/index.ts
+++ packages/types/src/index.ts
@@ -1,1 +1,2 @@
-export type { CustomType } from "./types.js";
+export * from "./medusa-error/medusa-error-types.js";
+export * from "./types.js";
---
+++ packages/types/src/medusa-error/medusa-error-types.ts
@@ -0,0 +1,33 @@
+// map of Medusa error types to HTTP status codes
+export const MedusaErrorTypes: Record<string, Record<string, number>> = {
+  database_error: {
+    "MEDUSA.GENERIC_ERROR": 500,
+  },
+  duplicate_error: {
+    "MEDUSA.DUPLICATE_ERROR": 409,
+  },
+  invalid_argument: {
+    "MEDUSA.INVALID_ARGUMENT": 400,
+  },
+  unexpected_state: {
+    "MEDUSA.UNEXPECTED_STATE": 400,
+  },
+  invalid_data: {
+    "MEDUSA.INVALID_DATA": 400,
+  },
+  unauthorized: {
+    "MEDUSA.UNAUTHENTICATED": 401,
+  },
+  not_found: {
+    "MEDUSA.NOT_FOUND": 404,
+  },
+  not_allowed: {
+    "MEDUSA.NOT_ALLOWED": 403,
+  },
+  conflict: {
+    "MEDUSA.CONFLICT": 409,
+  },
+  payment_authorization_error: {
+    "MEDUSA.PAYMENT_AUTHORIZATION_ERROR": 402,
+  },
+} as const;
