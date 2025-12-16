555a263c96b05a6beefc9b9bddd05972a0b890a1
Kyung Lee <kyung.lee.official@outlook.com>
2025-12-16 08:08:20 +0800

    chore: add comment for async translations usage in Home component

--- apps/next-intl-app/src/app/[locale]/page.tsx
+++ apps/next-intl-app/src/app/[locale]/page.tsx
@@ -3,6 +3,8 @@ import { useTranslations } from "next-intl";
 
 export default function Home() {
   const t = useTranslations("HomePage");
+  /* In case of async components, you can use the awaitable getTranslations function instead: */
+  //   const t = await getTranslations('HomePage');
 
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
