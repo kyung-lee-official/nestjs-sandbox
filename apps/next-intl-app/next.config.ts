e72342d187a75289832bdbe8eeb35d35900a5941
Kyung Lee <kyung.lee.official@outlook.com>
2025-12-16 14:47:20 +0800

    feat: support MDX without plugins

--- apps/next-intl-app/next.config.ts
+++ apps/next-intl-app/next.config.ts
@@ -1,10 +1,20 @@
+import createMDX from "@next/mdx";
 import type { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
 
 const nextConfig: NextConfig = {
+  /* config `pageExtensions` to include markdown and MDX files */
+  pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
   /* config options here */
   reactCompiler: true,
 };
 
 const withNextIntl = createNextIntlPlugin();
-export default withNextIntl(nextConfig);
+const withMDX = createMDX({
+  extension: /\.mdx?$/,
+  options: {
+    // MDX options here
+  },
+});
+
+export default withNextIntl(withMDX(nextConfig));
---
+++ apps/next-intl-app/src/app/zh-CN/layout.tsx
@@ -0,0 +1,69 @@
+import type { Metadata } from "next";
+import "../globals.css";
+import { NextIntlClientProvider, useMessages } from "next-intl";
+import { getTranslations, setRequestLocale } from "next-intl/server";
+import { Footer } from "@/components/footer/Footer";
+import { Header } from "@/components/header/Header";
+import { locales } from "@/i18n/routing";
+
+/* https://nextjs.org/docs/app/api-reference/functions/generate-static-params */
+export function generateStaticParams() {
+  return locales.map((locale) => ({ locale }));
+}
+
+export async function generateMetadata({
+  params: { locale },
+}: any): Promise<Metadata> {
+  const t = await getTranslations({ locale, namespace: "HomePage" });
+
+  return {
+    metadataBase: new URL("https://manual.chitubox.com"),
+    icons: {
+      icon: "/logo.png",
+    },
+    title: {
+      template: `%s | ${t("title")}`,
+      default: t("title"),
+    },
+    description: t("description"),
+    openGraph: {
+      title: {
+        template: `%s | ${t("title")}`,
+        default: t("title"),
+      },
+      description: t("description"),
+      images: "https://manual.chitubox.com/images/docs/og_logo.png",
+    },
+  };
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  setRequestLocale("zh-CN");
+
+  const messages = useMessages();
+
+  return (
+    <html lang="zh-CN">
+      <body className="font-[NotoSansCJKsc-Regular]">
+        <NextIntlClientProvider messages={messages}>
+          <div id="root-portal"></div>
+          <Header />
+          <main
+            className="flex min-h-svh
+			text-neutral-900 dark:text-neutral-200
+			dark:bg-black"
+          >
+            <div className="w-full min-w-0 max-w-[900px] p-4 mx-auto">
+              {children}
+            </div>
+          </main>
+          <Footer />
+        </NextIntlClientProvider>
+      </body>
+    </html>
+  );
+}
