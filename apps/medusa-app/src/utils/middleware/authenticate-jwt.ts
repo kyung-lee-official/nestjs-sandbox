412c46d97767f182f786c8a7174f974a4d84b844
Kyung Lee <kyung.lee.official@outlook.com>
2025-12-22 15:24:06 +0800

    feat: rename auth middleware

--- apps/medusa-app/src/utils/middleware/authenticate-jwt.ts
+++ apps/medusa-app/src/utils/middleware/authenticate-jwt.ts
@@ -22,7 +22,7 @@ interface JwtContext {
   exp?: number;
 }
 
-export const authenticateCookieJwt = (
+export const authenticateJwt = (
   actorType: string | string[],
   authType: "bearer" | ["bearer"] = "bearer",
   options: { allowUnregistered?: boolean } = {},
