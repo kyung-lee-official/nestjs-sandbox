1a8f8cdce4f0a3dbe35f1f9abf143ff88128a0a1
Kyung Lee <kyung.lee.official@outlook.com>
2026-01-08 15:07:07 +0800

    refactor: simplify axios error handling and improve type definitions

--- apps/next-app/src/app/medusa/axios-error-handling-for-medusa/axios-client.ts
+++ apps/next-app/src/app/medusa/axios-error-handling-for-medusa/axios-client.ts
@@ -1,55 +1,32 @@
-import type { HttpErrorResponse } from "@repo/types";
+import type { HttpErrorData } from "@repo/types";
 import axios, {
   type AxiosError,
-  type AxiosRequestConfig,
-  type InternalAxiosRequestConfig,
+  AxiosHeaders,
+  type AxiosResponse,
 } from "axios";
 import { useAuthStore } from "@/stores/auth";
-
-interface CustomRequestConfig extends AxiosRequestConfig {
-  skipInterceptor?: boolean;
-  bearerToken?: string; // for registerCustomer and resetPassword APIs
-}
 
 const instance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_MEDUSA_BACKEND_URL,
   withCredentials: true,
 });
 
-instance.interceptors.request.use(
-  async (config: InternalAxiosRequestConfig & CustomRequestConfig) => {
-    config.headers["x-publishable-api-key"] =
-      process.env.NEXT_PUBLIC_MEDUSA_PUBLISHABLE_KEY;
-
-    if (config.bearerToken) {
-      config.headers.Authorization = `Bearer ${config.bearerToken}`;
-    }
-
-    return config;
-  },
-);
-
 instance.interceptors.response.use(
   (response) => response,
-  (error: AxiosError<HttpErrorResponse>) => {
+  (error: AxiosError<HttpErrorData>) => {
     const status = error.response?.status;
-    const errorData = error.response?.data?.error;
+    const errorData = error.response?.data;
     const errorCode = errorData?.code;
-    const skipInterceptor =
-      (error.config as CustomRequestConfig)?.skipInterceptor ?? false;
 
     if (
-      status === 404 &&
       error.response?.headers["content-type"] === "text/html; charset=utf-8"
     ) {
       return Promise.reject({
-        error: {
-          code: "SYSTEM.ENDPOINT_NOT_FOUND",
-          message: `The requested endpoint ${error.config?.url} was not found on the server.`,
-          details: {},
-          timestamp: errorData?.timestamp,
-        },
-      } as HttpErrorResponse);
+        code: "SYSTEM.ENDPOINT_NOT_FOUND",
+        message: `The requested endpoint ${error.config?.url} was not found on the server.`,
+        details: {},
+        timestamp: errorData?.timestamp,
+      });
     }
 
     if (status === 401) {
@@ -57,79 +34,105 @@ instance.interceptors.response.use(
       signOut();
 
       return Promise.reject({
-        error: {
-          code: errorCode || "AUTH.UNAUTHORIZED",
-          message: errorData?.message,
-          details: {},
-          timestamp: errorData?.timestamp,
-        },
-      } as HttpErrorResponse);
+        code: errorCode || "AUTH.UNAUTHORIZED",
+        message: errorData?.message,
+        details: {},
+        timestamp: errorData?.timestamp,
+      });
     }
 
     if (errorData) {
       return Promise.reject({
-        error: {
-          code: errorData.code,
-          message: errorData.message,
-          details: errorData.details,
-          timestamp: errorData.timestamp,
-        },
-      } as HttpErrorResponse);
+        code: errorData.code,
+        message: errorData.message,
+        details: errorData.details,
+        timestamp: errorData.timestamp,
+      });
     }
 
     return Promise.reject({
-      error: {
-        code: "SYSTEM.UNKNOWN_ERROR",
-        message:
-          error.response?.status === 500
-            ? "Something went wrong on the server"
-            : error.message || "Network Error",
-        details: { error },
-        timestamp: new Date().toISOString(),
-      },
-    } as HttpErrorResponse);
+      code: "SYSTEM.UNKNOWN_ERROR",
+      message:
+        error.response?.status === 500
+          ? "Something went wrong on the server"
+          : error.message || "Network Error",
+      details: {},
+      timestamp: new Date().toISOString(),
+    });
   },
 );
 
-/**
- * 封装 GET 请求函数
- * @param url
- * @param config
- * @returns Promise<T>
- */
-export const get = <T>(url: string, config?: CustomRequestConfig): Promise<T> =>
-  instance
-    .get<T>(url, {
-      ...config,
-    })
-    .then((res) => res.data);
+async function get<T>(
+  url: string,
+  options?: {
+    params?: any;
+    withoutApiKey?: boolean;
+    apiKey?: string;
+  },
+): Promise<T> {
+  const res = await request<T>("GET", url, options);
+  return res.data;
+}
 
-/** 封装 POST 请求函数
- * @param url
- * @param data
- * @param config
- * @returns Promise<T>
- */
-export const post = <T>(
+async function post<T>(
   url: string,
   data?: any,
-  config?: CustomRequestConfig,
-): Promise<T> => instance.post<T>(url, data, config).then((res) => res.data);
+  options?: {
+    withoutApiKey?: boolean;
+    apiKey?: string;
+    header?: AxiosHeaders;
+  },
+): Promise<T> {
+  const res = await request<T>("POST", url, {
+    data,
+    withoutApiKey: options?.withoutApiKey,
+    apiKey: options?.apiKey,
+    header: options?.header,
+  });
+  return res.data;
+}
 
-/** 封装 DELETE 请求函数
- * @param url
- * @param config
- * @returns Promise<T>
- */
-export const del = async <T>(
+async function del<T>(
   url: string,
-  config?: CustomRequestConfig,
-): Promise<T> =>
-  instance
-    .delete<T>(url, {
-      ...config,
-    })
-    .then((res) => res.data);
+  data?: any,
+  options?: {
+    withoutApiKey?: boolean;
+    apiKey?: string;
+  },
+): Promise<T> {
+  const res = await request<T>("DELETE", url, {
+    data,
+    withoutApiKey: options?.withoutApiKey,
+    apiKey: options?.apiKey,
+  });
+  return res.data;
+}
 
-const api = { get, post, del };
-export default api;
+async function request<T>(
+  method: "GET" | "POST" | "PUT" | "DELETE",
+  url: string,
+  options?: {
+    data?: any;
+    params?: any;
+    withoutApiKey?: boolean;
+    apiKey?: string;
+    header?: AxiosHeaders;
+  },
+): Promise<AxiosResponse<T>> {
+  const headers = new AxiosHeaders();
+  headers.set("Content-Type", "application/json");
+  options?.withoutApiKey ||
+    headers.set(
+      "x-publishable-api-key",
+      options?.apiKey || process.env.NEXT_PUBLIC_MEDUSA_PUBLISHABLE_KEY,
+    );
+  return await instance.request<T>({
+    method,
+    url,
+    ...options,
+    headers: options?.header ? options.header.concat(headers) : headers,
+  });
+}
+
+const http = { get, post, del, request };
+export default http;
