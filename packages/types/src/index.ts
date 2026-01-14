912b9cf95cc470fade69de3c8e08e87ab22cb621
Kyung Lee <kyung.lee.official@outlook.com>
2026-01-14 10:51:55 +0800

    feat: add PayPal order and payment schemas for order creation and response handling

--- packages/types/src/index.ts
+++ packages/types/src/index.ts
@@ -1,5 +1,6 @@
 export * from "./http-error";
 export * from "./medusa-error/medusa-error-types";
+export * from "./payment/paypal-order-v2";
 export * from "./schemas";
 export * from "./types";
 export * from "./util-types/util-types";
---
+++ packages/types/src/payment/paypal-order-v2/types.ts
@@ -0,0 +1,115 @@
+export enum PayPalOrderQK {
+  GET_ORDER_BY_ID = "get_order_by_id",
+}
+
+export interface PayPalTokenResponse {
+  scope: string;
+  access_token: string;
+  token_type: string;
+  app_id: string;
+  expires_in: number;
+  nonce: string;
+}
+
+export interface PayPalAddress {
+  address_line_1: string;
+  address_line_2?: string;
+  admin_area_1: string;
+  admin_area_2: string;
+  postal_code: string;
+  country_code: string;
+}
+
+interface PayPalAmount {
+  currency_code: string;
+  value: string;
+}
+
+interface PayPalPurchaseUnit {
+  reference_id: string;
+  amount: PayPalAmount;
+}
+
+interface PayPalExperienceContext {
+  return_url: string;
+  cancel_url: string;
+}
+
+interface PayPalPaymentSource {
+  paypal: {
+    address: PayPalAddress;
+    email_address: string;
+    payment_method_preference: string;
+    experience_context: PayPalExperienceContext;
+  };
+}
+
+export interface CreateOrderRequest {
+  intent: "AUTHORIZE" | "CAPTURE";
+  purchase_units: PayPalPurchaseUnit[];
+  payment_source: PayPalPaymentSource;
+}
+
+export interface PayPalOrderResponse {
+  id: string;
+  status: string;
+  payment_source: {
+    paypal: {
+      email_address: string;
+      account_id: string;
+      account_status: string;
+      name: {
+        given_name: string;
+        surname: string;
+      };
+      phone_type: string;
+      address: PayPalAddress;
+    };
+  };
+  purchase_units: Array<{
+    reference_id: string;
+    amount: {
+      currency_code: string;
+      value: string;
+    };
+    payee: {
+      email_address: string;
+      merchant_id: string;
+    };
+    shipping: {
+      name: {
+        full_name: string;
+      };
+      address: PayPalAddress;
+    };
+  }>;
+  intent: string;
+  create_time: string;
+  update_time: string;
+  links: Array<{
+    href: string;
+    rel: string;
+    method: string;
+  }>;
+}
+
+export interface PayPalCaptureResponse {
+  id: string;
+  status: string;
+  purchase_units: Array<{
+    reference_id: string;
+    payments: {
+      captures: Array<{
+        id: string;
+        status: string;
+        amount: {
+          currency_code: string;
+          value: string;
+        };
+        final_capture: boolean;
+        create_time: string;
+        update_time: string;
+      }>;
+    };
+  }>;
+}
