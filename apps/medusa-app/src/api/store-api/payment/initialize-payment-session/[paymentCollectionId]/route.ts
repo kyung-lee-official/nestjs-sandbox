e8a88c11860f6e2903305b166d879665067051d9
Kyung Lee <kyung.lee.official@outlook.com>
2026-01-16 16:40:26 +0800

    chore: ...

--- apps/medusa-app/src/api/store-api/payment/initialize-payment-session/[paymentCollectionId]/route.ts
+++ apps/medusa-app/src/api/store-api/payment/initialize-payment-session/[paymentCollectionId]/route.ts
@@ -4,12 +4,6 @@ import {
   createPaymentSessionsWorkflow,
 } from "@medusajs/medusa/core-flows";
 import type { StoreInitializePaymentSession } from "@medusajs/types";
-import type { IntentType } from "@repo/types";
-
-type Data = {
-  intent: IntentType;
-  payment_collection_id: string;
-};
 
 export async function POST(req: MedusaRequest, res: MedusaResponse) {
   const paymentCollectionId = req.params.paymentCollectionId;
@@ -19,7 +13,7 @@ export async function POST(req: MedusaRequest, res: MedusaResponse) {
     input: {
       provider_id: provider_id,
       payment_collection_id: paymentCollectionId,
-      data: data as Data,
+      data: data,
     } as CreatePaymentSessionsWorkflowInput,
   });
 
